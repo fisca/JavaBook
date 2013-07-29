2fb6a8138402e3aa88a06e087dd422dc1fe15467
Suttipong Srikok <fisca2012@gmail.com>
2013-07-30 02:27:07 +0700

    Create ImplicitTest Project

---
+++ ImplicitTest/src/ImplicitTest.java
@@ -0,0 +1,10 @@
+public class ImplicitTest {
+    public static void main(String[] args) {
+        float firstNo=5;
+        int secondNo =0;
+        System.out.println("firstNo " + firstNo);
+        System.out.println("secondNo is " + secondNo);
+        firstNo = secondNo;
+        System.out.println("firstNo = " + firstNo);
+    }
+}
